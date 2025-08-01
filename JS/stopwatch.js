b21b5558b2fd17dd3868ab63b6a12db8d9ac7337
Yuri1031 <142709785+Yuri1031@users.noreply.github.com>
2025-08-01 16:28:21 +0900

    ストップウォッチ実装

---
+++ JS/stopwatch.js
@@ -0,0 +1,50 @@
+console.log('connected to stopwatch.js!')
+
+const stopwatch = document.getElementById("stopwatch");
+
+const startWatchBtn = document.querySelector(".start_stopwatch");
+const stopWatchBtn = document.querySelector(".stop_stopwatch");
+const resetWatchBtn = document.querySelector(".reset_stopwatch");
+
+let gapTime = 0;
+let startMs = null;
+let intervalWatchID;
+
+
+function countUp(){ 
+    if (intervalWatchID) return;
+    let startMs = Date.now() - gapTime;  // STARTを押した時点のtime
+    intervalWatchID = setInterval(() => {
+        const nowMs = Date.now(); // 経過後のtime
+        gapTime = nowMs - startMs;
+
+        const ms = gapTime % 1000;
+        const s = Math.floor(gapTime / 1000) % 60; 
+        const m = Math.floor((gapTime / 1000) / 60) % 60;
+        const h = Math.floor(gapTime / 3600000) % 60;
+
+        const formatMS = String(ms).padStart(3,'0');
+        const formatS = String(s).padStart(2,'0');
+        const formatM = String(m).padStart(2,'0');
+        const formatH = String(h).padStart(2,'0');
+
+        stopwatch.textContent = `${formatH}:${formatM}:${formatS}.${formatMS}`;
+    }, 10);         
+};
+
+startWatchBtn.addEventListener("click",() => {
+    countUp();
+});
+
+stopWatchBtn.addEventListener("click",() => {
+    clearInterval(intervalWatchID);
+    intervalWatchID = null;
+})
+
+resetWatchBtn.addEventListener("click", () => {
+    clearInterval(intervalWatchID);
+    gapTime = 0;
+    stopwatch.textContent = "00:00:00.000";
+    intervalWatchID = null;
+});
+
